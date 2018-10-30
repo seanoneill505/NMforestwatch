f35defdc0448410b88d1cff92cd9ceb87149b6d1
seanoneill505 <oneill505@gmail.com>
2018-10-30 10:39:32 -0600

    Update GEE_Processing.js
    Includes imports

--- js/GEE_Processing.js
+++ js/GEE_Processing.js
@@ -1,3 +1,20 @@
+var muni = ee.FeatureCollection("users/oneill505/tl_2015_35_place"),
+    L8 = ee.ImageCollection("LANDSAT/LC8_L1T_TOA"),
+    NM = /* color: #d63000 */ee.Geometry.Polygon(
+        [[[-109.09635377269097, 37.01279452169551],
+          [-109.12931275706597, 31.22376979419876],
+          [-108.10758424144097, 31.233164277806193],
+          [-108.10758424144097, 31.701694309067012],
+          [-106.23990846019097, 31.701694309067012],
+          [-106.23990846019097, 31.963043887687917],
+          [-102.92203736644097, 31.9444006199804],
+          [-102.93302369456597, 37.047876800141054]]]),
+    viz = {"opacity":1,"bands":["nd"],"min":0.10244220979511738,"max":0.4868767995387316,"palette":["beb261","8ffff9","1cd810"]},
+    imageVisParam = {"opacity":1,"bands":["nd"],"min":-0.3950178027153015,"max":0.5896815657615662,"palette":["beadab","ff0707"]},
+    imageVisParam2 = {"opacity":1,"bands":["nd"],"min":-0.2313025712966919,"max":0.5536771535873413,"palette":["bebebe","ff0303"]},
+    usfsnmaz = ee.FeatureCollection("ft:1ISdC2MnQItTj5fBZGMpWed-45rI3dot3t_N2qPVd"),
+    nmco = ee.FeatureCollection("ft:11dWiQkFMNxzP_xHfg98wz9paHoTjlsLRsVwSf0ZZ");
+
 var rgb_vis = {min: 0, max: 0.3, bands: ['B4', 'B3', 'B2']};
 function addNDVI(image) {
   var ndvi = image.normalizedDifference(['B5', 'B4']);
